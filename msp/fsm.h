0baa61ed9e1273157e0dd9c880db2c0d25fc5da6
Mirco Stelzer <mirco.stelzer@studenti.unitn.it>
2025-12-02 17:34:00 +0100

    States declaration

--- msp/fsm.h
+++ msp/fsm.h
@@ -1,6 +1,11 @@
 /*
  * fsm.h
  *
+ * This file contains the declaration
+ * of the finite state machine, its
+ * states and the events handled by
+ * the system
+ *
  *  Created on: Dec 2, 2025
  *      Author: mirco
  */
@@ -8,6 +13,26 @@
 #ifndef FSM_H_
 #define FSM_H_
 
+typedef enum {
+
+} Event_t;
+
+typedef enum {
+    INIT,
+    IDLE,
+    VEHICLE_ENTRY,
+    ENTRY_REFUSED,
+    ENTRY_ALLOWED,
+    VEHICLE_EXIT,
+    CLOSED,
+    STATES_NUM
+} State_t;
+
+
+typedef struct {
+    State_t state;
+    void (*state_function)(void);
+} Fsm_t;
 
 
 #endif /* FSM_H_ */
