5f5afa7e9173ebaa966e4ec1a5c1dea81af500e5
Mirco Stelzer <mirco.stelzer@studenti.unitn.it>
2026-01-04 17:01:07 +0100

    added init functions

---
+++ esp/main/init.h
@@ -0,0 +1,24 @@
+/**
+ * init.h
+ * 
+ * Initilization functions header file
+ * 
+ */
+
+
+
+#ifndef INIT_H
+#define INIT_H
+
+void hw_init();
+
+void camera_init();
+
+void ultrasonic_init();
+
+void weight_sensor_init();
+
+void wifi_init();
+
+
+#endif /* INIT_H */
